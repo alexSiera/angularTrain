60b7befe68e532ed61a818ef208cc6c79d690f91
alexanderPavlovtsev <alexander.pavlovtsev@fusion-tech.pro>
2021-05-12 18:30:32 +0300

    start 51 lesson

--- recipe-book-project/src/app/app.module.ts
+++ recipe-book-project/src/app/app.module.ts
@@ -1,11 +1,27 @@
+import { RecipesComponent } from './recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingListItemComponent } from './shopping/shopping-list/shopping-list-item/shopping-list-item.component';
+import { ShoppingListEditComponent } from './shopping/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeListItemСomponent } from './recipes/recipe-list/recipe-list-item/recipe-list-item.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListItemComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeListItemСomponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ recipe-book-project/src/app/recipes/recipe-list/recipe-list-item/recipe-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-recipe-list-item',
+  templateUrl: './recipe-list-item.component.html',
+  styleUrls: ['./recipe-list-item.component.css']
+})
+
+export class RecipeListItemСomponent {
+
+}
