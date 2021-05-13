77f9066534c0b363f7fbe154a2468e375d383678
alexanderPavlovtsev <alexander.pavlovtsev@fusion-tech.pro>
2021-05-13 19:09:06 +0300

    end 85 lesson

--- recipe-book-project/src/app/header/header.component.ts
+++ recipe-book-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,8 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent {
-
+  @Output('onMenuItemSelect') select = new EventEmitter<string>();
+  onSelect(type: string) {
+      this.select.emit(type);
+  }
 }
