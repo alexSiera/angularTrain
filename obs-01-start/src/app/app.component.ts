09fbe76324d5e501e87f64c3da3f30d63a9a8353
alexSiera <fa1nt.siera@gmail.com>
2021-05-18 01:23:59 +0300

    end 176 Lesson

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  userActivated = false;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 }
--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstIbsSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
-  }
+    // this.firstIbsSubscription = interval(1000)
+    //   .subscribe(c => {
+    //     console.log(c);
+    //   })
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count > 3) {
+          observer.error(new Error('Count is greter than 3'))
+        }
+        if (count > 4) {
+          observer.complete();
+        }
+        count++;
+      }, 1000)
+    })
+    customIntervalObservable
 
+
+    this.firstIbsSubscription = customIntervalObservable
+      .pipe(
+        filter(data => data !== 3),
+        map((data: number) => `Round: ${data + 1}`)
+      )
+      .subscribe(
+      data => console.log(data),
+      error => console.log(error),
+      () => console.log('Observer completed')
+    )
+  }
+  ngOnDestroy() {
+    this.firstIbsSubscription.unsubscribe();
+  }
 }
