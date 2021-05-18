dc2b9b6f7c36958ab177800869699157f7784e83
alexanderPavlovtsev <alexander.pavlovtsev@fusion-tech.pro>
2021-05-18 18:26:45 +0300

    end up 256 lesson

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { map } from 'rxjs/operators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,21 +8,53 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  basUrl = 'https://angular-train-3939d-default-rtdb.europe-west1.firebasedatabase.app/posts.json';
 
-  constructor(private http: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    //console.log(postData);
+    this.httpClient.post(
+      this.basUrl, postData
+    )
+    .subscribe(
+      (responseData) => {
+        console.log(responseData)
+      }
+    )
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+  private fetchPosts() {
+    this.httpClient
+      .get(this.basUrl)
+      .pipe(
+        map((responseData) => {
+          const postsArray = [];
+          for(const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key], id: key})
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe(
+        // ...
+        (data) => console.log(data)
+      )
   }
 }
